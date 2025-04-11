1253945915c52ad49ddb09be6de187b11e43a10f
Jens Böckmann <jensboe@outlook.de>
2025-04-11 21:53:38 +0200

    Less blinky

--- modellbahn/src/main.cpp
+++ modellbahn/src/main.cpp
@@ -12,13 +12,14 @@ int main()
     Board::Adapter_A::LedYellow::set(false);
     Board::Adapter_A::LedGreen::set(true);
 
+    Board::ExpantionBoard::Cs::set(false);
+    Board::ExpantionBoard::SpiMaster::transferBlocking(static_cast<uint8_t>(0b1000'0000));
+    Board::ExpantionBoard::Cs::set(true);
+
     while (true)
     {
         Board::Nucleo::LedBlue::toggle();
         static uint32_t counter(0);
-        Board::ExpantionBoard::Cs::set(false);
-        Board::ExpantionBoard::SpiMaster::transferBlocking(static_cast<uint8_t>(counter));
-        Board::ExpantionBoard::Cs::set(true);
         modm::delay(50ms);
         MODM_LOG_INFO << "Loop counter: " << (counter++) << modm::endl;
     }
